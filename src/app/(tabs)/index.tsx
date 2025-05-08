1868186fbbc323a3abb81bd314ff0b5bd347a27d
ben-1178 <benbinoy70@gmail.com>
2025-05-08 18:46:40 +0400

    setup nativewind

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -3,17 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Feed</Text>
+    <View>
+      <Text className='text-3xl font-bold text-red-500 p-2'>Feed</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
