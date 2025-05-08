4990e83cdde48d85354355936d56c7cce2464444
ben-1178 <benbinoy70@gmail.com>
2025-05-08 18:51:35 +0400

    path aliases

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,10 +1,9 @@
 import { StyleSheet, Text, View } from 'react-native';
 
-
 export default function HomeScreen() {
   return (
-    <View>
-      <Text className='text-3xl font-bold text-red-500 p-2'>Feed</Text>
+    <View className='flex-1 p-4'>
+      <Text className='text-3xl font-bold text-white p-2'>Hello World</Text>
     </View>
   );
 }
