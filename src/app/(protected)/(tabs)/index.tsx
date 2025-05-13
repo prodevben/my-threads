f1517f7bc185d033cc0b8f1b5eb9bb00fe45f280
ben-1178 <benbinoy70@gmail.com>
2025-05-13 16:38:05 +0400

    New post screen done

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,6 +1,7 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { dummyPosts } from '@/dummyData';
 import PostListItem from '@/components/PostListItem';
+import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
@@ -8,6 +9,11 @@ export default function HomeScreen() {
       data={dummyPosts}
       renderItem={({item}) => (
         <PostListItem post = {item}/>
+      )}
+      ListHeaderComponent={()=>(
+        <Link href='/new' className='text-blue-500 p-4 text-3xl text-center'>
+          New Post
+        </Link>
       )}
     />
   );
