6745fbe90d332102fb02b61dcf16841cc25209f7
ben-1178 <benbinoy70@gmail.com>
2025-05-13 17:19:21 +0400

    Auth UI (Signup and Login page)

--- src/app/(protected)/(tabs)/index.tsx
+++ src/app/(protected)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { dummyPosts } from '@/dummyData';
 import PostListItem from '@/components/PostListItem';
